8a4a17efa2a382c8bb177468f6b9de8c11b2d06b
javanjuju <105160680+javanjuju@users.noreply.github.com>
2023-05-03 10:42:18 +0100

    Add Html webpack plugin to genarate index.html Add js_modules folder for all modules Modify webpack.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode:'development',
-  watch: true,
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devtool: 'inline-source-map',
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'The Barite',
+		}),
+	],
+	devtool: 'inline-source-map',
 };
